0c90b29f36880adbdd4be8559376a0b5e7459a85
harshsbajwa <harsh.bajwa@mail.utoronto.ca>
2022-11-14 11:37:37 -0500

    Changed customization in Pet for consistency with Customization entity

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,51 @@
+package entities;
+
+public class User {
+    int Points;
+    Pet MyPet;
+
+    public User (int points) {
+        this.Points = points;
+    }
+    
+    public User (int points, Pet pet) {
+        this.Points = points;
+        this.MyPet = pet;
+    }
+    
+    public int getPoints(){
+        return Points;
+    }
+
+    public Pet getPet() {
+        return MyPet;
+    }
+
+    public Checklist getTODOlist() {
+        return TODOlist;
+    }
+
+    public Checklist getDONElist() {
+        return DONElist;
+    }
+
+    public void GainPoints (int points) {
+        Points += points;
+    }
+
+    public Boolean LosePoints (int points) {
+        if (Points < points) {
+            return false;
+        }
+        Points -= points;
+        return true;
+    }
+
+    public void setPet (Pet somePet) {
+        this.MyPet = somePet;
+    }
+
+    public String toString () {
+        return "User with points: " + this.Points + ", Pet: " + this.MyPet.getName();
+    }
+}
