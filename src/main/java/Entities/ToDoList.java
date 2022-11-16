5280e6224c9ac84cfe138bd0c8aa888fb5888a64
stephanie <stephanie.saab@mail.utoronto.ca>
2022-11-16 18:50:00 -0500

    Created the ToDoList class, added its private attribute (taskList) and its two public methods (addTask and removeTask). Javadocumentation for the class, its attribute and each method is added as well.

---
+++ src/main/java/Entities/ToDoList.java
@@ -0,0 +1,30 @@
+package Entities;
+
+import java.util.List;
+import java.util.Comparator;
+
+
+public class ToDoList {
+    // ToDoList is a public Class that contains a list of tasks that the user has yet to complete. Tasks can be added
+    // or removed from this taskList, the taskList is not sorted but a sorted list based on different attributes
+    // can be returned for display purposes.
+
+    private List<Task> taskList; //A list of tasks yet to be completed by the User
+
+    /**
+     * Add the given task to this ToDoList's taskList.
+     * @param newTask
+     */
+    public void addTask(Task newTask) {
+        this.taskList.add(newTask);
+    }
+
+    /**
+     * Remove the task with the given name from this
+     * @param oldTask
+     */
+    public void removeTask(Task oldTask) {
+        this.taskList.remove(oldTask);
+    }
+
+}
