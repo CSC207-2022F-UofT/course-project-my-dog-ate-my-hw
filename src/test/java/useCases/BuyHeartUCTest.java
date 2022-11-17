1265bf2c3741913c84041e18bd79cf649bc22d4f
Harsh Bajwa <90355783+harshsbajwa@users.noreply.github.com>
2022-11-17 00:03:12 -0500

    Update BuyHeartUCTest.java
    updated documentation for consistency and organization

--- src/test/java/useCases/BuyHeartUCTest.java
+++ src/test/java/useCases/BuyHeartUCTest.java
@@ -1,4 +1,23 @@
 package useCases;
 
 public class BuyHeartUCTest {
+
+    /** Create a new pet and user
+     * Set user points as 10, pet begins with 10 health
+     * Remove 5 hearts
+     * Then test BuyHeartUC.buyHeart() by buying 3 hearts
+     * User should have 7 points (10 - 3 = 7)
+     * Pet should have 8 hearts (5 + 3 = 8)
+     */
+    @Test
+    public void BuyHeartTest() {
+        ImageIcon skin = new ImageIcon();
+        Customization customization = new Customization();
+        Pet pet = new Pet("Name", skin, customization);
+        User user = new User(10, pet);
+        pet.ouch(5);
+        BuyHeartUC.buyHeart(user, 3);
+        Assertions.assertEquals(user.getPoints(), 7);
+        Assertions.assertEquals(pet.getCurrHealth(), 8);
+    }
 }
