75ab849a1e86224af5a7b9b904e6eadf9bd2d643
serenahonekin <97366619+serenahonekin@users.noreply.github.com>
2022-11-19 01:37:49 -0500

    reformatted

--- src/main/java/useCase/CreateTaskUC.java
+++ src/main/java/useCase/CreateTaskUC.java
@@ -15,29 +15,35 @@ public class CreateTaskUC {
         this.task = task;
     }
 
-    /** Creates a task.
-     * @param name the task's name
-     * @param course the task's belonging course
+    /**
+     * Creates a task.
+     *
+     * @param name     the task's name
+     * @param course   the task's belonging course
      * @param deadline the task's deadline
      * @param priority the task's priority level
      */
-    public void createTask(String name, String course, LocalDateTime deadline, Priority priority){
+    public void createTask(String name, String course, LocalDateTime deadline, Priority priority) {
         task = new Task(name, course, deadline, priority);
     }
 
-    /** Creates a task.
-     * @param name the task's name
-     * @param course the task's belonging course
-     * @param deadline the task's deadline
-     * @param priority the task's priority level
-     * @param assignmentType the task's type of assignment
+    /**
+     * Creates a task.
+     *
+     * @param name            the task's name
+     * @param course          the task's belonging course
+     * @param deadline        the task's deadline
+     * @param priority        the task's priority level
+     * @param assignmentType  the task's type of assignment
      * @param studyTechniques the task's ideal study technique
      */
-    public void createCustomTask(String name, String course, LocalDateTime deadline, Priority priority, String studyTechniques, String assignmentType){
+    public void createCustomTask(String name, String course, LocalDateTime deadline, Priority priority, String studyTechniques, String assignmentType) {
         task = new CustomTask(name, course, deadline, priority, assignmentType, studyTechniques);
     }
 
-    /** Adds task to the To Do list.
+    /**
+     * Adds task to the To Do list.
+     *
      * @param task the task to be added to the to do list
      */
     public void addToTDL(Task task) {
