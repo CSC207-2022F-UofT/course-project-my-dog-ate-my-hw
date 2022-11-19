8474d841403906b131b34c666e209c4ad1b48d1a
serenahonekin <97366619+serenahonekin@users.noreply.github.com>
2022-11-19 01:29:01 -0500

    Added inherited constructor from Task

--- src/main/java/entities/CustomTask.java
+++ src/main/java/entities/CustomTask.java
@@ -1,22 +1,30 @@
 package entities;
 
+import java.time.LocalDateTime;
+
 public class CustomTask extends Task {
-    private String AssignmentType;
-    private String StudyTechniques;
+    private String assignmentType;
+    private String studyTechniques;
+
+    public CustomTask(String name, String course, LocalDateTime deadline, Priority priority, String assignmentType, String studyTechniques) {
+        super(name, course, deadline, priority);
+        this.assignmentType = assignmentType;
+        this.studyTechniques = studyTechniques;
+    }
 
     public String getAssignmentType() {
-        return AssignmentType;
+        return assignmentType;
     }
 
     public void setAssignmentType(String assignmentType) {
-        AssignmentType = assignmentType;
+        this.assignmentType = assignmentType;
     }
 
     public String getStudyTechniques() {
-        return StudyTechniques;
+        return studyTechniques;
     }
 
     public void setStudyTechniques(String studyTechniques) {
-        StudyTechniques = studyTechniques;
+        this.studyTechniques = studyTechniques;
     }
 }
