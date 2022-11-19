c37e2559ef81b016641211540fe3ae4ec8e2d8cd
serenahonekin <97366619+serenahonekin@users.noreply.github.com>
2022-11-18 23:44:26 -0500

    Added all getters and setters to Task

---
+++ src/main/java/entities/Task.java
@@ -0,0 +1,60 @@
+package entities;
+
+import java.util.Date;
+
+public abstract class Task {
+    public enum Priority {
+        LOW,
+        MEDIUM,
+        HIGH
+    }
+
+    ;
+
+    private Date deadline;
+    private String name;
+    private String course;
+    private Priority priority;
+    private boolean completion;
+
+    public Date getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(Date deadline) {
+        this.deadline = deadline;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public boolean isCompletion() {
+        return completion;
+    }
+
+    public void setCompletion(boolean completion) {
+        this.completion = completion;
+    }
+}
