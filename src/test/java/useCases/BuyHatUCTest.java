783ac218cba24244afde2958576f16bb12475ee9
chrischin1 <christian.bongalon@mail.utoronto.ca>
2022-11-21 05:10:27 -0500

    Began implementing BuyHatUCTest.java

--- src/test/java/useCases/BuyHatUCTest.java
+++ src/test/java/useCases/BuyHatUCTest.java
@@ -8,4 +8,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BuyHatUCTest {
+
+    /** Create a new pet, user, and hat
+     * Set user points as 10 and hat price as 7
+     * Then test BuyHatUC.buyHat() by buying purchasing the hat
+     * User should have 3 points (10 - 7 = 3)
+     * The hat should be successfully equipped on the pet
+     */
+
 }
