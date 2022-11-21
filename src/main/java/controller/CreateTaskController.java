8cb74ae22c806af6ef27270c742ecd52e66ab3b0
serenahonekin <97366619+serenahonekin@users.noreply.github.com>
2022-11-21 17:48:04 -0500

    Added a converter from string to LocalDateTime

--- src/main/java/controller/CreateTaskController.java
+++ src/main/java/controller/CreateTaskController.java
@@ -4,17 +4,25 @@ import useCase.CreateTaskUC;
 import entities.Task;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class CreateTaskController {
     Task task;
 
-    public void PerformCreateTask(String name, String course, LocalDateTime deadline, Priority priority){
+    public void performCreateTask(String name, String course, LocalDateTime deadline, Priority priority){
         CreateTaskUC createTaskUC = new CreateTaskUC();
         task = createTaskUC.createTask(name, course, deadline, priority);
         createTaskUC.addToTDL(task);
     }
 
-    public CreateTaskController() {
+    public CreateTaskController(String name, String course, String deadline, Priority priority) {
+        LocalDateTime dueDate = convertString(deadline);
+        performCreateTask(name, course, dueDate, priority);
+    }
+
+    private LocalDateTime convertString(String str){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return LocalDateTime.parse(str, formatter);
     }
 
 }
