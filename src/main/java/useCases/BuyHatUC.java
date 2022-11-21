b35ef0dd05325ee4b0e59095a47cf7130725480b
chrischin1 <christian.bongalon@mail.utoronto.ca>
2022-11-21 04:19:10 -0500

    Completed implementation of BuyHatUC.java

--- src/main/java/useCases/BuyHatUC.java
+++ src/main/java/useCases/BuyHatUC.java
@@ -1,6 +1,5 @@
 package useCases;
 
-import entities.Hat;
 import entities.Item;
 import entities.Pet;
 import entities.User;
@@ -8,15 +7,14 @@ import entities.Customization;
 
 public class BuyHatUC {
 
-    Hat hat;
-
     /**
      * Allows the user ot buy a hat and updates their points and pet accordingly.
      * @param user the User's name
      * @param hat the Hat the user wants to purchase
+     * @param customization the possible customizations assigned to the given pet
      */
     public void buyHat(User user, Item hat, Customization customization){
-        Pet pet = user.getPet();
+        Pet pet = user.getPet(); // Customization not yet assigned to pet class
 
         if (user.getPoints() >= hat.price) {
             user.LosePoints(hat.price);
--- src/test/java/useCases/BuyHatUCTest.java
+++ src/test/java/useCases/BuyHatUCTest.java
@@ -1,4 +1,11 @@
 package useCases;
 
+import entities.Item;
+import entities.Pet;
+import entities.User;
+import entities.Customization;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class BuyHatUCTest {
 }
