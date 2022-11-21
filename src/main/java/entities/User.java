e8b5bc08895e6ab47147f979a8af6862a01351cf
emilycoffen <emily.coffen@outlook.com>
2022-11-20 19:28:17 -0500

    added back user
    Signed-off-by: emilycoffen <emily.coffen@outlook.com>

---
+++ src/main/java/entities/User.java
@@ -0,0 +1,48 @@
+package entities;
+
+public class User {
+    int Points;
+    Pet MyPet;
+    Checklist TODOlist;
+    Checklist DONElist;
+
+    public int getPoints() {
+        return Points;
+    }
+
+    public Pet getPet() {
+        return MyPet;
+    }
+
+    public Checklist getTODOlist() {
+        return TODOlist;
+    }
+
+    public Checklist getDONElist() {
+        return DONElist;
+    }
+
+    public void GainPoints(int points) {
+        Points += points;
+    }
+
+    public Boolean LosePoints(int points) {
+        if (Points < points) {
+            return false;
+        }
+        Points -= points;
+        return true;
+    }
+
+    public void setPet(Pet somePet) {
+        this.MyPet = somePet;
+    }
+
+    public void setTODOlist(Checklist someList) {
+        this.TODOlist = someList;
+    }
+
+    public void setDONElist(Checklist someList) {
+        this.DONElist = someList;
+    }
+}
