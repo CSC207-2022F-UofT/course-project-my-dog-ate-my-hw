627c80497c5703f047ac97b240a06810422db76e
serenahonekin <97366619+serenahonekin@users.noreply.github.com>
2022-11-21 17:21:39 -0500

    changed assignmentType from string to type enum class

--- src/main/java/entities/CustomTask.java
+++ src/main/java/entities/CustomTask.java
@@ -3,20 +3,20 @@ package entities;
 import java.time.LocalDateTime;
 
 public class CustomTask extends Task {
-    private String assignmentType;
+    private AssignmentType assignmentType;
     private String studyTechniques;
 
-    public CustomTask(String name, String course, LocalDateTime deadline, Priority priority, String assignmentType, String studyTechniques) {
+    public CustomTask(String name, String course, LocalDateTime deadline, Priority priority, AssignmentType assignmentType, String studyTechniques) {
         super(name, course, deadline, priority);
         this.assignmentType = assignmentType;
         this.studyTechniques = studyTechniques;
     }
 
-    public String getAssignmentType() {
+    public AssignmentType getAssignmentType() {
         return assignmentType;
     }
 
-    public void setAssignmentType(String assignmentType) {
+    public void setAssignmentType(AssignmentType assignmentType) {
         this.assignmentType = assignmentType;
     }
 
