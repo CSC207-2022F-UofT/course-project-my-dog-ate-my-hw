1980d6447ca4c7b68ea5646f68637358486b62bf
chrischin1 <christian.bongalon@mail.utoronto.ca>
2022-11-21 21:24:14 -0500

    Changed condition for buyHat to check if hat is locked first

--- src/main/java/useCases/BuyHatUC.java
+++ src/main/java/useCases/BuyHatUC.java
@@ -8,14 +8,14 @@ import entities.Customization;
 public class BuyHatUC {
 
     /**
-     * Allows the user ot buy a hat and updates their points and pet accordingly.
+     * Allows the user to buy a hat and updates their points and pet accordingly.
      * @param user the User's name
      * @param hat the Hat the user wants to purchase
      */
     public static void buyHat(User user, Item hat){
         Customization customization = user.getPet().getCustomization();
 
-        if (user.getPoints() >= hat.price) {
+        if (user.getPoints() >= hat.price && !hat.unlocked) {
             user.LosePoints(hat.price);
         }
         if (!customization.isCurrentlyEquipped()){
