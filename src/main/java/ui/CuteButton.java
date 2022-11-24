eff5b925d0f3c1d1dad8497baeb2b65cab992c8b
emilycoffen <emily.coffen@outlook.com>
2022-11-24 13:54:31 -0500

    Starting to separate main ui components and create view models.
    Signed-off-by: emilycoffen <emily.coffen@outlook.com>

---
+++ src/main/java/ui/CuteButton.java
@@ -0,0 +1,20 @@
+package ui;
+
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public abstract class CuteButton extends JButton {
+
+    public CuteButton(Color background, Color textColor, Border border, String label, Dimension preferedSize, Font font){
+        this.setText(label);
+        this.setBackground(background);
+        this.setBorder(border);
+        this.setForeground(textColor);
+        this.setFont(font);
+        this.setPreferredSize(preferedSize);
+    }
+
+}
