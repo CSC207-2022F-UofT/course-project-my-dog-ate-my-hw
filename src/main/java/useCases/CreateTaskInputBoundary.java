617d47a3c8a61ec979d6d79e20828b1efdfb6084
serenahonekin <97366619+serenahonekin@users.noreply.github.com>
2022-11-30 17:42:32 -0500

    added rest of method headers from CreateTaskUC

--- src/main/java/useCases/CreateTaskInputBoundary.java
+++ src/main/java/useCases/CreateTaskInputBoundary.java
@@ -1,6 +1,8 @@
 package useCases;
 
+import entities.AssignmentType;
 import entities.Priority;
+import entities.Task;
 
 import javax.swing.*;
 import java.time.LocalDateTime;
@@ -8,6 +10,12 @@ import java.time.LocalDateTime;
 public interface CreateTaskInputBoundary {
 
     void createTask(String name, String course, LocalDateTime deadline, Priority priority);
+
+    void createTask(String name, String course, LocalDateTime deadline, Priority priority, String studyTechniques, AssignmentType assignmentType);
+
+    public void addToTDL(Task task);
+
+    public Task getTask();
 
     void refreshTask();
 }
