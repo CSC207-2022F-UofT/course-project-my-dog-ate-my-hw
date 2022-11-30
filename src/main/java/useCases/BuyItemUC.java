c4c056e31b5d16704146e5d2b889708b45e91a9b
harshsbajwa <harsh.bajwa@mail.utoronto.ca>
2022-11-30 17:10:58 -0500

    untracked files preventing pull from branch 1

---
+++ src/main/java/useCases/BuyItemUC.java
@@ -0,0 +1,39 @@
+package useCases;
+
+import entities.Item;
+import entities.User;
+import entities.Customization;
+
+public class BuyItemUC {
+
+    /**
+     * Allows the user to buy an item and updates their points and pet accordingly.
+     * @param user the User's name
+     * @param item the item the user wants to purchase
+     */
+    public static void buyHat(User user, Item item){
+        Customization customization = user.getPet().getCustomization();
+
+        int price = item.getPrice();
+        boolean unlocked = item.isUnlocked();
+
+
+        if (user.getPoints() >= price && !unlocked) {
+            user.LosePoints(price);
+        }
+        if (!customization.isCurrentlyEquipped()){
+            if (unlocked) {
+                item.setUnlocked(true);
+                customization.equip(item);
+            }
+        } else if (unlocked) {
+            item.setUnlocked(true);
+            customization.dequip();
+            customization.equip(item);
+        }
+        else {
+            customization.dequip();
+            customization.equip(item);
+        }
+    }
+}
