d778cbc20b3830f34909bfd39da1a526d5726228
stephanie <stephanie.saab@mail.utoronto.ca>
2022-11-30 19:25:52 -0500

    Added the checkUniqueTaskName in the ToDoList. The addTask method has been changed to return a boolean, true if the task was succesfully added and false otherwise.

--- src/main/java/entities/Checklist.java
+++ src/main/java/entities/Checklist.java
@@ -4,42 +4,43 @@ import java.util.ArrayList;
 public abstract class Checklist {
         /**
          * An abstract class containing a list of tasks that and can be manipulated. Tasks can be added or removed from
-         * the checklist. Tasks can also be searched from the checklist. The checklist can be converted to a list of
-         * strings for viewing purposes.
+         * the task list. Tasks can also be searched from the tasklist. The task list can be returned for
+         * viewing purposes.
          */
 
-        private List<Task> checklist;
+        private List<Task> taskList;
 
         public Checklist() {
-            this.checklist = new ArrayList() <>;
+            this.taskList = new ArrayList();
         }
 
 
         /**
-         * Add the given task to checklist. Do not sort the checklist.
+         * Add the given task to taskList. Do not sort the taskList.
          * @param task
          */
-        public void addTask(Task task) {
-            this.checklist.add(task);
+        public boolean addTask(Task task) {
+            this.taskList.add(task);
+            return true;
         }
 
         /**
-         * Remove the given task from the checklist.
+         * Remove the given task from the taskList.
          * @param task
          */
         public void removeTask(Task task) {
-            this.checklist.remove(task);
+            this.taskList.remove(task);
         }
 
         /**
-         * Return the task in the checklist that has the given name. The names of tasks are unique.
+         * Return the task in the taskList that has the given name. The names of tasks are unique.
          * @param taskName : the name of the task being searched for.
          * @return : a task with the given name or throw an AbsentTaskNameException if there is no task with
          * the given name.
          */
 
         public Task searchFor(String taskName) throws AbsentTaskNameException {
-                for (Task task : checklist) {
+                for (Task task : taskList) {
                     if (task.getName() == taskName) {
                         return task;
                     }
@@ -48,10 +49,10 @@ public abstract class Checklist {
             }
 
         /**
-         * Return a clone of the checklist.
-         * @return : A clone of the list the checklist attribute.
+         * Return the taskList.
+         * @return : The taskList attribute.
          */
-        public abstract List<String> toList() {
-
+        public List<Task> getTaskList() {
+            return this.taskList;
             }
 }
--- src/main/java/entities/ToDoList.java
+++ src/main/java/entities/ToDoList.java
@@ -1,47 +1,59 @@
 package entities;
+/*
 
 import java.util.List;
 import java.util.ArrayList;
+*/
 
-public class ToDoList {
-    // ToDoList is a public Class that contains a list of tasks that the user has yet to complete. Tasks can be added
-    // or removed from this taskList, the taskList is not sorted but a sorted list based on different attributes
-    // can be returned for display purposes.
-    private List<Task> taskList; //A list of tasks yet to be completed by the User
-
+public class ToDoList extends Checklist {
     /**
-     * Initialize an empty ToDOList with no tasks in its taskList.
-     */
-    public ToDoList() {
-        this.taskList = new ArrayList();
-    }
-
-    /**
-     * Add the given task to this ToDoList's taskList.
-     * @param newTask
-     */
-    public void addTask(Task newTask) {
-        this.taskList.add(newTask);
-    }
-
-    /**
-     * Remove the task with the given name from this
-     * @param oldTask
+     * ToDoList is a public Class that contains a list of tasks that the user has yet to complete, it is a child class
+     *  of Checklist. In addition to the inherited methods from Checklist, the ToDoList can be sorted by the deadlines
+     *  of the task and the priorities of the task.
+     *
+     *
+     *
+     *  ====== Representation invariant ========
+     *
+     * 1) Every task in the taskList must have a unique name.
      */
 
-    public void removeTask(Task task){this.taskList.remove(task); }
 
-    public void removeTask(String taskName) {
-        this.taskList.remove(this.searchFor(taskName));
-    }
-
-    public Task searchFor(String taskName){
-        for (Task task : this.taskList){
-            if(task.getName().equals(taskName)){
-                return task;
+    /**
+     * Check if the task has a unique name, meaning it does not have the same name as any other tasks in the taskList.
+     *
+     * @param newTask : The given task to be added to the taskList.
+     * @return : true if the given task has a unique name and false otherwise.
+     */
+    public boolean checkUniqueName(Task newTask) {
+        for (Task addedTask : this.getTaskList()) {
+            if (addedTask.getName() == newTask.getName()) {
+                return false;
             }
         }
-        return null;
+        return true;
     }
 
+    @Override
+    /**
+     * Add the given task to the taskList if it has a unique name.
+     * @param newTask : the given task to add to the taskList.
+     * @return boolean : return true if the task was successfully added to the taskList. Return false if the task does
+     * not have a unique name and needs a new name.
+     */
+     public boolean addTask(Task newTask) {
+         if (checkUniqueName(newTask)) {
+             super.addTask(newTask);
+         }
+         return false;
+    }
+
+    /**
+     * Return a taskList sorted by the deadline of each task. In order of the soonest deadline to the latest
+     * deadline.
+     * @return List<Task> : a list of the tasks in taskList sorted by the deadlines of the tasks.</Task>
+     */
+
+     public void sortedDeadlinesToDo() {
+     }
 }
