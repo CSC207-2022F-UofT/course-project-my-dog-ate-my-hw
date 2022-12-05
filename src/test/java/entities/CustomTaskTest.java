19ef94850c41124122435b10633e4641d3fc6917
serenahonekin <97366619+serenahonekin@users.noreply.github.com>
2022-12-04 20:42:32 -0500

    added tests for custom tasks

---
+++ src/test/java/entities/CustomTaskTest.java
@@ -0,0 +1,64 @@
+package entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class CustomTaskTest {
+
+    /**
+     * Created a new custom task.
+     * Set task name as "testing", course as "csc207", deadline as 2022-11-30, and priority as high.
+     * Also assigned it to Project assignment type with study technique of "chunk time".
+     * Then tested respective getters and setters for the assignment type and study technique.
+     */
+    @Test
+    public void CreateCustomTaskTest() {
+        LocalDateTime deadline = LocalDateTime.parse("2022-11-30");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDateTime dateTime = LocalDateTime.parse("2022-11-30", formatter);
+        CustomTask task = new CustomTask("testing", "csc207", deadline, Priority.HIGH,
+                AssignmentType.PROJECT, "chunk time");
+        Assertions.assertEquals("testing", task.getName());
+        Assertions.assertEquals("csc207", task.getCourse());
+        Assertions.assertEquals(dateTime, task.getDeadline());
+        Assertions.assertEquals("HIGH", task.getPriority().name());
+        Assertions.assertEquals("PROJECT", task.getAssignmentType().name());
+        Assertions.assertEquals("chunk time", task.getStudyTechniques());
+    }
+
+    @Test
+    public void GetAssignmentTypeTest() {
+        LocalDateTime deadline = LocalDateTime.parse("2022-11-30");
+        CustomTask task = new CustomTask("reflection", "csc207", deadline, Priority.HIGH,
+                AssignmentType.REFLECTION, "chunk time");
+        Assertions.assertEquals("REFLECTION", task.getAssignmentType().name());
+    }
+    @Test
+    public void SetAssignmentTest() {
+        LocalDateTime deadline = LocalDateTime.parse("2022-11-30");
+        CustomTask task = new CustomTask("reflection", "csc207", deadline, Priority.HIGH,
+                AssignmentType.REFLECTION, "chunk time");
+        task.setAssignmentType(AssignmentType.ESSAY);
+        Assertions.assertEquals("ESSAY", task.getAssignmentType().name());
+    }
+
+    @Test
+    public void GetStudyTechniqueTest() {
+        LocalDateTime deadline = LocalDateTime.parse("2025-12-30");
+        CustomTask task = new CustomTask("reflection", "csc207", deadline, Priority.HIGH,
+                AssignmentType.REFLECTION, "just do it");
+        Assertions.assertEquals("just do it", task.getStudyTechniques());
+    }
+
+    @Test
+    public void SetStudyTechniqueTest() {
+        LocalDateTime deadline = LocalDateTime.parse("2025-12-30");
+        CustomTask task = new CustomTask("reflection", "csc207", deadline, Priority.HIGH,
+                AssignmentType.REFLECTION, "just do it");
+        task.setStudyTechniques("think before you write");
+        Assertions.assertEquals("think before your write", task.getStudyTechniques());
+    }
+}
