7639841a89949722f2c74b1008fb85ee65f87067
serenahonekin <97366619+serenahonekin@users.noreply.github.com>
2022-12-05 16:48:47 -0500

    added documentation to ModifyTaskUC

--- src/main/java/useCases/ModifyTaskUC.java
+++ src/main/java/useCases/ModifyTaskUC.java
@@ -10,20 +10,23 @@ import java.time.LocalDateTime;
  * The modify task use case takes in the task name, deadline, course, priority and assignment type
  * to change the task in the to-do list.
  */
-public class ModifyTaskUC {
+public class ModifyTaskUC implements ModifyTaskInputBoundary{
     private Task task;
 
     /**
      * This constructs the ModifyTaskUC by taking in the string of a task name then finding the task
      * in the to-do list.
+     *
      * @param taskName the task name
      */
+
     public ModifyTaskUC(String taskName) {
         this.task = todo.searchFor(taskName);
     }
 
     /**
      * Changes the task's name.
+     *
      * @param newName task's new name
      */
     public void changeName(String newName) {
@@ -32,6 +35,7 @@ public class ModifyTaskUC {
 
     /**
      * Changes the task's associated course.
+     *
      * @param course task's new course
      */
     public void changeCourse(String course) {
@@ -40,6 +44,7 @@ public class ModifyTaskUC {
 
     /**
      * Changes the task's priority.
+     *
      * @param priority task's priority
      */
     public void changePriority(Priority priority) {
@@ -48,6 +53,7 @@ public class ModifyTaskUC {
 
     /**
      * Changes the task's deadline
+     *
      * @param date task's deadline
      */
     public void changeDeadline(LocalDateTime date) {
@@ -56,11 +62,19 @@ public class ModifyTaskUC {
 
     /**
      * Changes the task's assignment type.
+     *
      * @param assignmentType the task's assignment type.
      */
     public void changeAssignmentType(AssignmentType assignmentType) {
         task.setAssignmentType(assignmentType);
     }
 
+    /**
+     * Calls on factory to create a refresher.
+     */
+    public void refreshTask() {
+        RefresherFactory factory = new RefresherFactory();
+        factory.createRefresher("Tasklist").refresh();
+    }
 }
 
