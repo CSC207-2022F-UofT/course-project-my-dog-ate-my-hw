8ea6f633bc8d2fe71af3a90514fe3dcb257846d6
harshsbajwa <harsh.bajwa@mail.utoronto.ca>
2022-12-05 20:18:38 -0500

    added User UC, TaskUIDS, PetUIDS, Refresher, RefresherFactory, TasklistOutputBoundary, and MainOutputBoundary

---
+++ src/main/java/useCases/MainOutputBoundary.java
@@ -0,0 +1,8 @@
+package useCases;
+
+// main output boundary
+public interface MainOutputBoundary {
+    PetOutputBoundary getPetOutputBoundary();
+
+    TasklistOutputBoundary getTasklistOutputBoundary();
+}
