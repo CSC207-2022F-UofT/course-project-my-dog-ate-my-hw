0768ed3d9be446d66f0dca2f5d25eed4cc8f71fd
stephanie <stephanie.saab@mail.utoronto.ca>
2022-12-07 03:02:08 -0500

    Added a TaskPanel class with a method that displays panels using a loop so we ay display tasks in the ToDoList. The aesthetics of the display can be improved but the components are there.

--- src/main/java/ui/TaskPanel.java
+++ src/main/java/ui/TaskPanel.java
@@ -1,38 +1,56 @@
 package ui;
 
 import javax.swing.*;
+import javax.swing.border.*;
 import java.awt.*;
 
 public class TaskPanel extends JFrame {
 
-    public TaskPanel(){
+    public TaskPanel() {
         init();
     }
 
-    private void init(){
-        JPanel panel = new JPanel();
-        panel.setBackground(Color.CYAN);
+    private void init() {
+        //JPanel panel = new JPanel();
         JPanel panel2 = new JPanel();
-        panel2.setBackground(Color.LIGHT_GRAY);
-
-        JLabel label = new JLabel("This is a label");
-
+        //panel.setBackground(Color.LIGHT_GRAY);
+        panel2.setBackground(Color.pink);
 
 
         this.setLayout(new BorderLayout());
-        this.add(panel, BorderLayout.CENTER);
-        this.add(panel2, BorderLayout.LINE_START);
+        this.setMinimumSize(new Dimension(100, 100));
+        panel2.setLayout(new BoxLayout(panel2, BoxLayout.PAGE_AXIS));
+       // JLabel label = new JLabel("This is a label");
+       // JLabel label2 = new JLabel("This is another label");
+
+        this.setLayout(new BorderLayout());
+        //this.add(panel, BorderLayout.CENTER);
+        this.add(panel2, BorderLayout.CENTER);
+
+        makeBoxPanels(10, panel2);
+
     }
 
+    private void makeBoxPanels(int x, JPanel container) {
+        for (int i = 0; i < x; i++) {
+            JPanel panel = new JPanel();
+            JCheckBox checkBox = new JCheckBox();
+            checkBox.setHorizontalAlignment(JLabel.LEFT);
+            panel.add(new JLabel("TaskName"));
+            panel.add(new JLabel("TaskCourse"));
+            panel.add(new JLabel("TaskType"));
+            panel.add(new JLabel("TaskDeadline"));
+            panel.add(new JLabel("TaskPriority"));
+            panel.setBorder(new LineBorder(Color.BLACK));
+            container.add(panel);
+            container.add(checkBox);
+        }
+    }
 
     public static void main(String[] args) {
         TaskPanel a = new TaskPanel();
         a.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        a.setPreferredSize(new Dimension(1000, 500));
-        a.setMinimumSize(new Dimension(1000, 500));
-        a.setResizable(false);
+        a.setPreferredSize(new Dimension(100, 100));
         a.setVisible(true);
-
-
     }
 }
