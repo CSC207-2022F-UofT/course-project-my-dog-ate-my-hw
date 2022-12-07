152a83bfba618008b434b12d16e8ab501b2f98ed
XinyingXu <XinyingXu@users.noreply.github.com>
2022-12-07 18:56:40 -0500

    Update ToDoList.java

--- src/main/java/entities/ToDoList.java
+++ src/main/java/entities/ToDoList.java
@@ -1,35 +1,73 @@
 package entities;
+/*
 
 import java.util.List;
 import java.util.ArrayList;
+*/
 
-public class ToDoList {
-    // ToDoList is a public Class that contains a list of tasks that the user has yet to complete. Tasks can be added
-    // or removed from this taskList, the taskList is not sorted but a sorted list based on different attributes
-    // can be returned for display purposes.
-    private List<Task> taskList; //A list of tasks yet to be completed by the User
+import java.util.concurrent.ThreadLocalRandom;
+
+public class ToDoList extends Checklist {
+    /** ToDoList is a public Class that contains a list of tasks that the user has yet to complete, it is a child class
+     *  of Checklist. In addition to the inherited methods from Checklist, the ToDoList can be sorted by the deadlines
+     *  of the task and the priorities of the task.
+     *  ====== Representation invariant ========
+     *
+     * 1) Every task in the taskList must have a unique name.
+     */
+
 
     /**
-     * Initialize an empty ToDOList with no tasks in its taskList.
+     * Check if the task has a unique name, meaning it does not have the same name as any other tasks in the taskList.
+     *
+     * @param newTask : The given task to be added to the taskList.
+     * @return : true if the given task has a unique name and false otherwise.
      */
-    public ToDoList() {
-        this.taskList = new ArrayList<>();
+    public boolean checkUniqueName(Task newTask) {
+        for (Task addedTask : this.getTaskList()) {
+            if (addedTask.getName().equals(newTask.getName())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    /** Add the given task to the taskList if it has a unique name.
+     * @param newTask : the given task to add to the taskList.
+     * @return boolean : return true if the task was successfully added to the taskList. Return false if the task does
+     * not have a unique name and needs a new name.
+     */
+     public boolean addTask(Task newTask) {
+         if (checkUniqueName(newTask)) {
+             super.addTask(newTask);
+         }
+         return false;
     }
 
     /**
-     * Add the given task to this ToDoList's taskList.
-     * @param newTask the task to be added to the taskList.
+     * Return the task in the taskList that has the given name. The names of tasks are unique.
+     * @param taskName : the name of the task being searched for.
+     * @return : a task with the given name or throw an AbsentTaskNameException if there is no task with
+     * the given name.
      */
-    public void addTask(Task newTask) {
-        this.taskList.add(newTask);
+
+    public Task searchFor(String taskName) throws AbsentTaskNameException {
+        for (Task task : super.getTaskList()) {
+            if (task.getName().equals(taskName)) {
+                return task;
+            }
+        }
+        throw new AbsentTaskNameException();
     }
 
     /**
-     * Remove the task with the given name from this taskList.
-     * @param oldTask the task to be removed from the taskList.
+     * Randomize a task in the list
+     * @return Task at random index in the taskList
      */
-    public void removeTask(Task oldTask) {
-        this.taskList.remove(oldTask);
+    public Task randomTask() {
+        int randomNum = ThreadLocalRandom.current().nextInt(0, super.getTaskList().size());
+        return super.getTaskList().get(randomNum);
     }
 
     /**
@@ -40,7 +78,7 @@ public class ToDoList {
      */
     public int pastDeadLineSum() {
         int sumOfLate = 0;
-        for (Task task: this.taskList) {
+        for (Task task: this.getTaskList()) {
             sumOfLate += task.pastDeadline();
         }
         return sumOfLate;
