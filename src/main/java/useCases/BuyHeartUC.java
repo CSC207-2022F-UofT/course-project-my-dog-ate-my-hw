644fdca6477f01acd05034ce62d7091bbc5647c2
harshsbajwa <harsh.bajwa@mail.utoronto.ca>
2022-12-07 20:29:25 -0500

    Made changes to AdoptPetUC, BuyHeartUC, PetShopUIController, and documentation changes to UserUC. Added AdoptPetInputBoundary, BuyHeartController, CheckerUC, PetOutputBoundary, PetRefresher, and TasklistRefresher

--- src/main/java/useCases/BuyHeartUC.java
+++ src/main/java/useCases/BuyHeartUC.java
@@ -1,25 +1,35 @@
 package useCases;
 
-import entities.User; // Need user points and user pet
-import entities.Pet; // Need Pet.getMaxHealth() in case we change this value
-
 public class BuyHeartUC {
-    
     /**
     * Updates the User's balance and the corresponding Pet's health (1:1)
     * @param hearts the number of hearts to heal (cost)
     * @param user the User's name
     */
-    public static void buyHeart(User user, int hearts){
+    public static void buyHeart(entities.User user, int hearts){
     
-        Pet pet = user.getPet();
-        
+        entities.Pet pet = user.getPet();
+
         if(user.getPoints() > 0 &&
                 pet.getCurrHealth() > 0 &&
-                pet.getCurrHealth() < Pet.getMaxHealth()){
+                pet.getCurrHealth() < entities.Pet.getMaxHealth()){
             user.LosePoints(hearts);
             pet.heal(hearts);
         }
+    }
+
+    /**
+     * Calls the User UseCase for a User and then calls buyHeart(user, hearts)
+     * @param hearts the number of hearts to heal (cost)
+     */
+    public static void buyHeart(int hearts){
+        User user = User.u();
+        buyHeart(user, hearts);
+    }
+
+    public void refresh(){
+        RefresherFactory factory = new RefresherFactory();
+        factory.createRefresher("Pet").refresh();
     }
 
 }
---
+++ src/main/java/useCases/CheckerUC.java
@@ -0,0 +1,14 @@
+package useCases;
+
+public class CheckerUC {
+    /**
+     * Checker Use case interactor.
+     * When the user opens the program this will check if any deadlines are past their due date and remove health
+     * from the pet accordingly.
+     */
+
+    public void checkDeadlines(){
+
+    }
+
+}
