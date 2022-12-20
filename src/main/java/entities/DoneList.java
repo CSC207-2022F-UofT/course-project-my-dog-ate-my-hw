5530bd087940bd76b1093d8268f0666e38806756
stephanie <stephanie.saab@mail.utoronto.ca>
2022-12-20 18:46:14 -0500

    Finished test coverage for Item entity class. Removed redundant Unlocked method from Item and BuyItemUC.

--- src/main/java/entities/DoneList.java
+++ src/main/java/entities/DoneList.java
@@ -6,6 +6,11 @@ package entities;
  */
 public class DoneList extends Checklist{
 
+    /**
+     * Add the given task to the DoneList's taskList.
+     * Mark the given task as completed.
+     * @param task : The given task to be added to the DoneList's taskList.
+     */
     @Override
     public boolean addTask(Task task) {
         task.setCompletion(true);
