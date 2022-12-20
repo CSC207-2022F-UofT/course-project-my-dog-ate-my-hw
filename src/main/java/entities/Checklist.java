d6eb768edba6298921c86f88eb19244ec5c7c5df
stephanie <stephanie.saab@mail.utoronto.ca>
2022-12-20 17:46:30 -0500

    Fixed documentation for Checklist class

--- src/main/java/entities/Checklist.java
+++ src/main/java/entities/Checklist.java
@@ -3,15 +3,19 @@ package entities;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * An abstract class containing a list of tasks that and can be manipulated. Tasks can be added or removed from
+ * the task list. Tasks can also be searched from the tasklist. The task list can be returned for
+ * viewing purposes.
+ */
 public abstract class Checklist {
-    /**
-     * An abstract class containing a list of tasks that and can be manipulated. Tasks can be added or removed from
-     * the task list. Tasks can also be searched from the tasklist. The task list can be returned for
-     * viewing purposes.
-     */
 
+    // === Private Instance Attributes ===
     private List<Task> taskList;
 
+    /**
+     * Construct a checklist given no arguments.
+     */
     public Checklist() {
         this.taskList = new ArrayList<>();
     }
